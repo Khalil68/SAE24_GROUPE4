c192fcf97d30cc9063ec99d5cda578a545082487
JLKayser <pro.daoudi.khalil@gmail.com>
2022-06-21 21:26:15 +0200

    Version 2 + Models

--- SAE24/mySAE24/models.py
+++ SAE24/mySAE24/models.py
@@ -1,30 +1,26 @@
 from django.db import models
 
-#____________________________________________________________
-class capteur1(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    nom_capteur = models.CharField(max_length=100)
+class capteur(models.Model):
+    nom = models.CharField(max_length=100)
     piece = models.CharField(max_length=100)
-    emplacement_capteur = models.CharField(max_length=100)
-
+    emplacement = models.CharField(max_length=100)
 
     def __str__(self):
-        chaine = f"{self.id}"
+        chaine = f"{self.nom}"
         return chaine
 
-    #def dictionnaire(self):
-     #   return {"nom": self.nom, "descriptif": self.descriptif}
-#____________________________________________________________
-class capteur2(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    nom_capteur = models.CharField(max_length=100)
-    piece = models.CharField(max_length=100)
-    emplacement_capteur = models.CharField(max_length=100)
+    def dico(self):
+        return {"nom": self.nom,"piece": self.piece, "emplacement": self.emplacement}
 
+
+class data(models.Model):
+    data = models.CharField(max_length=200)
+    timestamp = models.CharField(max_length=200)
+    capteur = models.ForeignKey("capteur", on_delete=models.CASCADE,blank=False)
 
     def __str__(self):
-        chaine = f"{self.id}"
+        chaine = self.data + "," + self.timestamp + "," + capteur.objects.get(id=self.capteur).piece + " | " + capteur.objects.get(id=self.capteur).nom
         return chaine
 
-    #def dictionnaire(self):
-     #   return {"nom": self.nom, "descriptif": self.descriptif}
+    def dico(self):
+        return {"nom": self.data,"piece": self.timestamp, "emplacement": self.capteur}
